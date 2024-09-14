e8668fc5b693c0f7e2e89be6593ed20083c0361b
Stults Mundi <bmg.stultusmundi@gmail.com>
2024-09-14 13:51:36 +0300

    Fix create intended behaviour

--- osiris/opcodes.py
+++ osiris/opcodes.py
@@ -202,7 +202,7 @@ def get_ins_cost(opcode):
         return GCOST["Gjumpdest"]
     elif opcode == "SHA3":
         return GCOST["Gsha3"]
-    elif opcode == ("CREATE", "CREATE2"):
+    elif opcode in ("CREATE", "CREATE2"):
         return GCOST["Gcreate"]
     elif opcode in ("CALL", "CALLCODE"):
         return GCOST["Gcall"]
